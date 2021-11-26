0c0addb937c8c9d20a9f067738547fb6222b31ea
秋雨落 <i@rain.cx>
2021-11-27 00:04:59 +0800

    :sparkles: Change default server port.
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/al/yn/synccraft/data/Config.java
+++ src/main/java/al/yn/synccraft/data/Config.java
@@ -4,9 +4,6 @@ public class Config {
     public String directory = "synccraft_mods";
 
     public String serverName = "SyncCraft";
-    
-    public String server = "127.0.0.1";
 
-    // Timeout in seconds.
-    public long timeout = 90;
+    public String server = "127.0.0.1:25565";
 }
